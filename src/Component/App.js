8a9ba4433acd6a5b049676af7539139c7831a20d
Abhinav Saurav <abhinavsaurav1@gmail.com>
2020-08-14 16:46:15 +0530

    Videolist and items showing up

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,21 +1,27 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
+	state = { videos: [] };
+
 	onTermSubmit = async (term) => {
 		console.log(term);
-		await youtube.get("/search", {
+		const response = await youtube.get("/search", {
 			params: {
 				q: term,
 			},
 		});
+		console.log(response.data);
+		this.setState({ videos: response.data.items });
 	};
 
 	render() {
 		return (
 			<div className="ui container">
 				<SearchBar onFormSubmit={this.onTermSubmit} />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
