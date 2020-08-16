2580f8469f68eba1b2f86eb20a15b84e32c11b0f
Abhinav Saurav <abhinavsaurav1@gmail.com>
2020-08-16 19:48:39 +0530

    [feat] Style changes

--- src/Component/VideoDetail.js
+++ src/Component/VideoDetail.js
@@ -10,7 +10,7 @@ const VideoDetail = ({ video }) => {
 	return (
 		<div className="ui segment">
 			<div className="ui embed">
-				<iframe src={videoSrc} />
+				<iframe title="video player" src={videoSrc} />
 			</div>
 			<h4 className="ui header">
 				<div>{video.snippet.title}</div>
